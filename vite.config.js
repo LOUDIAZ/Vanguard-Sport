ec4ebe7cac709ab5dc00c9e0501aff5f5dd539cf
lourdesdiaz <lourdesdiaz507@gmail.com>
2023-07-27 14:54:29 -0300

    pages

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "https://github.com/LOUDIAZ/Vanguard-Sport.git",
+  base: "https://loudiaz.github.io/Vanguard-Sport/",
 })
